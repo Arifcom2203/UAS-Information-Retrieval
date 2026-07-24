4e1cc3c2c5fd0d2e7f7ea4902deb156111921c3c
Arifcom2203 <bloodarep8@gmail.com>
2026-07-24 15:36:43 +0700

    Add files via upload

---
+++ scrapy_project/scrapy_project/spiders/books_spider.py
@@ -0,0 +1,37 @@
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+
+    allowed_domains = ["books.toscrape.com"]
+
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response):
+
+        books = response.css("article.product_pod")
+
+        for book in books:
+
+            yield {
+
+                "title": book.css("h3 a::attr(title)").get(),
+
+                "price": book.css("p.price_color::text").get(),
+
+                "availability": book.css(
+                    "p.instock.availability::text"
+                ).getall()[-1].strip(),
+
+                "rating": book.css("p.star-rating::attr(class)").get().replace(
+                    "star-rating ", ""
+                ),
+
+            }
+
+        next_page = response.css("li.next a::attr(href)").get()
+
+        if next_page:
+
+            yield response.follow(next_page, callback=self.parse)
